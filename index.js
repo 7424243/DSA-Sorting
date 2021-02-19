ab5592c2dd4fa50a666829d4533b9c508a14b98e
Sam Sheets <sam.sheets94@gmail.com>
2021-02-19 10:01:16 -0700

    completed question #5

--- index.js
+++ index.js
@@ -102,4 +102,77 @@ console.log(mergeSort(ARRAY))
 /* ===== 5. =====
 Given a Linked List, sort the linked list using merge sort. You will need your linked list class from previous lesson to create the list and use all of its supplemental functions to solve this problem.
 */
-
+function display(SLL) {
+    let currNode = SLL.head
+    while(currNode !== null) {
+        console.log(currNode.value)
+        currNode = currNode.next
+    }
+}
+function mergeSortList(list) {
+    let currNode = list.head
+    if(currNode.next === null) {
+        return list
+    }
+    let length = 1
+    while(currNode.next !==null) {
+        length++
+        currNode = currNode.next
+    }
+    const middle = Math.floor(length / 2)
+    let leftList = splitList(list, 0, middle)
+    let rightList = splitList(list, middle, length)
+    leftList = mergeSortList(leftList)
+    rightList = mergeSortList(rightList)
+    return mergeLists(leftList, rightList)
+}
+function splitList(list, start, end) {
+    let currNode = list.head
+    if(currNode === null) {
+        return
+    }
+    const returnList = new LinkedList()
+    let i = 0
+    while(currNode !== null) {
+        if(i >= start && i < end) {
+            returnList.insertLast(currNode.value)
+        }
+        i++
+        currNode = currNode.next
+    }
+    return returnList
+}
+function mergeLists(leftList, rightList) {
+    const mergedList = new LinkedList()
+    let currLeft = leftList.head
+    let currRight = rightList.head
+    while(currLeft && currRight) {
+        if(currLeft.value <= currRight.value) {
+            mergedList.insertLast(currLeft.value)
+            currLeft = currLeft.next
+        } else {
+            mergedList.insertLast(currRight.value)
+            currRight = currRight.next
+        }
+    }
+    while(currLeft) {
+        mergedList.insertLast(currLeft.value)
+        currLeft = currLeft.next
+    }
+    while(currRight) {
+        mergedList.insertLast(currRight.value)
+        currRight = currRight.next
+    }
+    return mergedList
+}
+function main() {
+    let list = new LinkedList()
+    list.insertFirst(4)
+    list.insertLast(5)
+    list.insertLast(1)
+    list.insertLast(3)
+    list.insertLast(0)
+    const sorted = mergeSortList(list)
+    display(sorted)
+}
+main()
